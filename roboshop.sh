ca94a37298d8faac052dfba2bbc5941cdc7d9ecb
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-25 17:50:49 +0530

    message

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ SG_ID="sg-083d903c3fd30a343" # Replace with your SG ID
 
 for instance in $@
 do
-  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-083d903c3fd30a343 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-083d903c3fd30a343 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
   # if frontend is not there then it will take private IP
   if [ $instance != "frontend" ]; then

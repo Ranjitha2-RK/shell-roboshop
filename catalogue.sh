e5400d6defe00efd7015eb022deb59a49c8ea0e2
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-25 23:40:48 +0530

    message

--- catalogue.sh
+++ catalogue.sh
@@ -42,7 +42,7 @@ if [ $? -ne 0 ]; then
   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
   VALIDATE $? "Creating system user"
 else
-  echo -e "User already exit .... $Y SKIPPING $N"
+  echo -e "User already exit ... $Y SKIPPING $N"
 fi
 
 mkdir -p /app

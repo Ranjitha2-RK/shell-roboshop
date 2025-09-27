677e8e15856761726a0bb3ab847debf37859028e
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-27 15:40:32 +0530

    message

--- catalogue-1.sh
+++ catalogue-1.sh
@@ -76,8 +76,9 @@ VALIDATE $? "Copy mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
 
-INDEX=$(mongosh mongodb.daws86s.sbs --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [ $INDEX -le 0 ]; then
+INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')" | xargs)
+
+if [[ -z "$INDEX" || "$INDEX" -le 0 ]]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
 else

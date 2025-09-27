c8687a1fdbcae259a4f52552dc95df88bead5b48
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-27 19:30:12 +0530

    message

--- redis.sh
+++ redis.sh
@@ -44,3 +44,7 @@ VALIDATE $? "Enabling Redis"
 
 systemctl start redis &&>>LOG_FILE
 VALIDATE $? "Starting Redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo  -e "Script executed in: $Y $TOTAL_TIME Seconds $N"

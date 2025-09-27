1e6ed532b6c5a179856267a8cb839b2d374098e2
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-27 18:59:54 +0530

    message

--- catalogue.sh
+++ catalogue.sh
@@ -80,7 +80,7 @@ VALIDATE $? "Install MongoDB client"
 #mongosh --host mongodb.daws86s.sbs </app/db/master-data.js
 #cat /var/log/shell-roboshop/catalogue.log | grep -A 20 "Load Catalogue Products"
 
-INDEX=$(mongosh --host mongodb.daws86s.sbs --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh mongodb.daws86s.sbs --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 
 if [ "$INDEX" -le 0 ]; then
   mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE

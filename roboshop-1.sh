b62379976da7db6c369936ee13950a088afc6c1c
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-28 12:59:15 +0530

    message

--- roboshop-1.sh
+++ roboshop-1.sh
@@ -12,10 +12,10 @@ do
   # if frontend is not there then it will take private IP
   if [ $instance != "frontend" ]; then
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-      RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.daws86s.sbs
+      RECORD_NAME=$instance.$DOMAIN_NAME #mongodb.daws86s.sbs
   else
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-      RECORD_NAME="$DOMAIN_NAME" #daws86s.sbs
+      RECORD_NAME=$DOMAIN_NAME #daws86s.sbs
   fi
 
   echo "$instance: $IP"

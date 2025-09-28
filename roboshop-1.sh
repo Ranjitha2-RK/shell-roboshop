cbf794da8732ef1e35acf1699ca8f96588ad0028
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-28 13:08:34 +0530

    message

--- roboshop-1.sh
+++ roboshop-1.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
-
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-083d903c3fd30a343" # Replace with your SG ID
-ZONE_ID="Z10394194WVGUSFSTERF" #REPALCE WITH YOUR id 
+SG_ID="sg-083d903c3fd30a343"
+ZONE_ID="Z10394194WVGUSFSTERF"
 DOMAIN_NAME="daws86s.sbs"
-
 for instance in $@
 do
-  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-083d903c3fd30a343 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
-
-  # if frontend is not there then it will take private IP
-  if [ $instance != "frontend" ]; then
-      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-      RECORD_NAME=$instance.$DOMAIN_NAME #mongodb.daws86s.sbs
-  else
-      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-      RECORD_NAME=$DOMAIN_NAME #daws86s.sbs
-  fi
-
-  echo "$instance: $IP"
-
-  aws route53 change-resource-record-sets \
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances
+    [0].InstanceId' --output text)
+    if [ $instance != "frontend" ]; then
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    RECORD_NAME=$instance.$DOMAIN_NAME
+    else
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    RECORD_NAME=$DOMAIN_NAME
+    fi
+    echo "$instance: $IP"
+    aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
